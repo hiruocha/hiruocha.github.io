30476f45d25bf106c3713eb79a2ea6df03dd6b4a
hiruocha <hiruocha@gmail.com>
2025-11-02 18:08:43 +0800

    chore(domain): 更改域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hiruocha.codeberg.page/",
+	site: "https://hiruocha.is-a.dev/",
 
 	base: "/",
 	trailingSlash: "always",
