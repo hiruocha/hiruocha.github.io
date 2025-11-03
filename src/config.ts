f53b031c2b270e41affd1a1fe6f0fda383756e30
hiruocha <hiruocha@gmail.com>
2025-11-03 17:31:40 +0800

    chore(字体): 删除自定义字体
    - 减小仓库体积
    - 对中文适配不友好

--- src/config.ts
+++ src/config.ts
@@ -168,13 +168,13 @@ export const siteConfig: SiteConfig = {
 	// 字体配置
 	font: {
 		zenMaruGothic: {
-			enable: true, // 启用全局圆体适合日语和英语，对中文适配一般
+			enable: false, // 启用全局圆体适合日语和英语，对中文适配一般
 		},
 		hanalei: {
 			enable: false, // 启用 Hanalei 字体作为全局字体，适合中文去使用
 		},
 	},
-	showLastModified: true, // 控制“上次编辑”卡片显示的开关
+	showLastModified: false, // 控制“上次编辑”卡片显示的开关
 };
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	enable: false, // 启用全屏壁纸功能,非Banner模式下生效
